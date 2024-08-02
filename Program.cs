171a0bd7901f46236ab18ef2b50a1d6175dcec30
Nikolai Voronin <nvoronin@outlook.com>
2024-08-02 09:46:56 +0300

    Polishing code

--- Program.cs
+++ Program.cs
@@ -8,89 +8,75 @@ const string ImageFilesMask = "*.jpg";
 const string SamplesFolderName = "Samples2"; // .../User/MyPictures + Samples/*.jpg
 const double ImageSimilarityThreshold = 90.0; // percents
 
-string _samplesFolderPath =
+string samplesFolderPath =
     Path.Combine(
-        Environment.GetFolderPath( Environment.SpecialFolder.MyPictures ),
+        Environment.GetFolderPath(
+            Environment.SpecialFolder.MyPictures ),
         SamplesFolderName );
 
-IImageHash _hashMethod = new PerceptualHash();
-List<ImageInfo> _pictures = [];
-var _imageHashComparer = new ImageHashComparer( ImageSimilarityThreshold );
+IImageHash hashMethod = new PerceptualHash();
+List<ImageInfo> pictures = [];
+var imageHashComparer =
+    new ImageHashComparer( ImageSimilarityThreshold );
 
 var sw = Stopwatch.StartNew();
 
-var precomputedHashesFileName = PrecomputedHashesFileName();
+var precomputedHashesFileName =
+    Path.Combine( samplesFolderPath, "hashs.json" );
+
 if (File.Exists( precomputedHashesFileName )) {
     Console.WriteLine( "\n+++ Restoring hashes..." );
 
     using var fs = File.OpenRead( precomputedHashesFileName );
-    _pictures = JsonSerializer.Deserialize<List<ImageInfo>>( fs )!;
+    pictures = JsonSerializer.Deserialize<List<ImageInfo>>( fs )!;
 }
 else {
     Console.WriteLine( "\n+++ Computing hashes..." );
 
     var _pictureFileNames =
         Directory.GetFiles(
-            _samplesFolderPath, ImageFilesMask,
+            samplesFolderPath, ImageFilesMask,
             SearchOption.AllDirectories );
 
     foreach (var picturePath in _pictureFileNames) {
         Console.WriteLine( $"+ {picturePath}" );
 
         using var stream = File.OpenRead( picturePath );
-        ulong hash = _hashMethod.Hash( stream );
+        ulong hash = hashMethod.Hash( stream );
 
-        _pictures.Add(
+        pictures.Add(
             new ImageInfo( picturePath, hash ) );
     }
 
-    using var fs = File.OpenWrite( PrecomputedHashesFileName() );
-    JsonSerializer.Serialize( fs, _pictures );
+    using var fs = File.OpenWrite( precomputedHashesFileName );
+
+    JsonSerializer.Serialize( fs, pictures );
 }
 
 Console.WriteLine( "\n+++ Chasing duplicates..." );
 
-var _groups = _pictures
-    .GroupBy( x => x.Hash, _imageHashComparer )
-    .Where(grp => grp.Count() > 1) // remove not duplicates
+var _groups =
+    pictures
+    .GroupBy( x => x.Hash, imageHashComparer )
+    .Where( grp => grp.Count() > 1 ) // remove not duplicates
     .Select( grp => grp.ToList() )
     .ToList();
 
-Console.WriteLine( $"\n+++ Similarity: max= {_imageHashComparer.MaxSim}% / min= {_imageHashComparer.MinSim}%" );
+Console.WriteLine( $"\n+++ Similarity: max= {imageHashComparer.MaxSim}% / min= {imageHashComparer.MinSim}%" );
 Console.WriteLine( $"\n+++ Duplicate Groups ({_groups.Count}):" );
 
 var groupIx = 0;
 foreach (var group in _groups) {
     Console.WriteLine( $"Group #{groupIx}" );
 
-    foreach (var iinfo in group) {
+    foreach (var iinfo in group)
         Console.WriteLine( $"\t{Path.GetFileName( iinfo.Path )}" );
-
-        CopyToSamplesRoot(
-            iinfo.Path,
-            $"{groupIx}--{Path.GetFileName( iinfo.Path )}" );
-    }
 
     groupIx++;
 }
 
 Console.WriteLine( $"\n+++ TOTAL: {sw.Elapsed}" );
 sw.Stop();
-
-string PrecomputedHashesFileName() =>
-    Path.Combine( _samplesFolderPath, "hashs.json" );
-
-void CopyToSamplesRoot( string fileName, string? renameTo = null )
-{
-    return; //////////////////////////////////////////////////////////////////////////// RETURN //
-    const bool Overwrite = true;
-
-    var name = Path.GetFileName( renameTo ?? fileName );
-    File.Copy(
-        fileName,
-        Path.Combine( _samplesFolderPath, name ),
-        Overwrite );
-}
 
 file class ImageInfo( string path, ulong hash )
 {
